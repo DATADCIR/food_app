0630b4b2071edb8f371d176452e06a68512a540d
parsa <parsaddbb@gmail.com>
2024-01-03 10:22:55 +0330

    fix sidebar linkes

--- app/(main)/_components/sidebar.jsx
+++ app/(main)/_components/sidebar.jsx
@@ -23,16 +23,16 @@ const Sidebar = () => {
             />
             <div className="nav_items flex flex-col gap-5">
               <Link href={"/"}>صفحه اصلی</Link>
-              <Link href={"/"} className="flex gap-1 items-center">
+              <Link href={"/store"} className="flex gap-1 items-center">
                 فروشگاه <Icons name="Down" />
               </Link>
-              <Link href={"/"} className="flex gap-1 items-center">
+              <Link href={"/contact-us"} className="flex gap-1 items-center">
                 تماس با ما <Icons name="Down" />
               </Link>
-              <Link href={"/"} className="flex gap-1 items-center">
+              <Link href={"/about-us"} className="flex gap-1 items-center">
                 درباره ما <Icons name="Down" />
               </Link>
-              <Link href={"/"} className="flex gap-1 items-center">
+              <Link href={"/food-menues"} className="flex gap-1 items-center">
                 منو غذا <Icons name="Down" />
               </Link>
             </div>
