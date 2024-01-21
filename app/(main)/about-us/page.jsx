81a8991713fffd4d9995cb0314db1dbb212be7f3
parsa <parsaddbb@gmail.com>
2024-01-21 20:36:46 +0330

    add gallery section to about us page

--- app/(main)/about-us/page.jsx
+++ app/(main)/about-us/page.jsx
@@ -1,3 +1,4 @@
+import Divider from "@/components/shared/dividers/divider";
 import Image from "next/image";
 import PageInspector from "../_components/PageInspector";
 const AboutUsPage = () => {
@@ -33,6 +34,52 @@ const AboutUsPage = () => {
         title={"سان دي >> درباره ما"}
         classes="text-yellow-light text-lg h-24 border-2 border-yellow-light my-10"
       />
+      <Divider tag={"گالری تصاویر"} classes="mb-10" />
+      <div className="flex flex-col lg:flex-row justify-center items-center p-10 bg-gray-light rounded-lg">
+        <div className="w-full lg:w-3/5 flex flex-col  gap-5">
+          <div className="flex w-1/2 gap-3">
+            <Image
+              alt="restaurent"
+              src="/images/Rectangle 55.png"
+              width={326}
+              height={248}
+              className=" brightness-25"
+            />
+            <Image
+              alt="restaurent"
+              src="/images/Rectangle 55.png"
+              width={326}
+              height={248}
+              className=" brightness-25"
+            />
+          </div>
+          <div className="flex w-1/2 gap-3">
+            <Image
+              alt="restaurent"
+              src="/images/Rectangle 55.png"
+              width={326}
+              height={248}
+              className=" brightness-25"
+            />
+            <Image
+              alt="restaurent"
+              src="/images/Rectangle 55.png"
+              width={326}
+              height={248}
+              className=" brightness-25"
+            />
+          </div>
+        </div>
+        <div className="w-full lg:w-2/5">
+          <Image
+            alt="restaurent"
+            src="/images/Rectangle 52.png"
+            width={440}
+            height={540}
+          />
+        </div>
+      </div>
+      <div className="flex "></div>
     </>
   );
 };
