c50301fd1575c161a0e4709ef778a71e40c0eca4
Amin <129054689+za-amin-rei@users.noreply.github.com>
2024-02-04 14:37:35 +0330

    Add light mode page contact us

--- app/(main)/contact-us/page.jsx
+++ app/(main)/contact-us/page.jsx
@@ -5,10 +5,10 @@ const ContactUsPage = () => {
   return (
     <>
       {" "}
-      <div className="w-full flex flex-col md:flex-row bg-gray-light pt-12">
+      <div className="w-full flex flex-col md:flex-row dark:bg-gray-light bg-[#E3E3E4] pt-12">
         <div className="flex flex-col justify-center  md:w-1/3 px-6 gap-5">
           <h1 className="text-2xl text-yellow-light">رستوران سان دي</h1>
-          <p className="text-white text-xl flex flex-col gap-3">
+          <p className="text-[#232323] dark:text-white text-xl flex flex-col gap-3">
             <span>اینجا میتونی با ما در ارتباط</span>
             <span>باشی و هر مشکل نظر یا سوالی راجب به کارمون</span>
             <span>دارین رو باهامون در میان بگذارین</span>
@@ -34,14 +34,14 @@ const ContactUsPage = () => {
         title={"سان دي >> تماس با ما"}
         classes="text-yellow-light text-lg h-24 border-2 border-yellow-light my-10 rounded-xl"
       />
-      <div className="flex flex-col md:flex-row items-center gap-5 bg-gray-light rounded-lg p-5 md:p-10 mb-10">
+      <div className="flex flex-col md:flex-row items-center gap-5 dark:bg-gray-light bg-[#E3E3E4] rounded-lg p-5 md:p-10 mb-10">
         <div className="md:w-3/5 flex flex-col gap-5 p-10 sm:p-20">
           <h1 className="text-yellow-light text-xl">فرم ارسال نظر</h1>
-          <p className="text-white text-lg">
+          <p className="text-[#232323] dark:text-white text-lg">
             شما میتوانید از این قسمت نظر انتقادات و هر مسئله ای را که صلاح
             میدانید با ما در ارتباط بگذارید.
           </p>
-          <p className="text-white text-lg">
+          <p className="text-[#232323] dark:text-white text-lg">
             {" "}
             توجه داشته باشید که میتوانید به غذاهای مورد علاقه تان نیز رای بدهید
             .
@@ -62,10 +62,10 @@ const ContactUsPage = () => {
           </button>
         </div>
       </div>
-      <div className="flex flex-col-reverse gap-10 md:flex-row pt-20 pb-28 bg-gray-light">
-        <div className="w-[90%] md:w-1/2 lg:w-2/5 flex flex-col gap-3 border-2 border-yellow-light outline outline-20 outline-body outline-offset-0 p-3 bg-body">
+      <div className="flex flex-col-reverse  gap-10 md:flex-row pt-20 pb-28 dark:bg-gray-light bg-[#E3E3E4]">
+        <div className="w-[90%] md:w-1/2 lg:w-2/5 flex flex-col gap-3 border-2 border-yellow-light outline outline-20 dark:outline-body outline-white  outline-offset-0 p-3 dark:bg-body bg-[#fff]">
           <div className="flex gap-3">
-            <div className="w-1/2 flex justify-center items-center bg-white rounded-lg py-8 ">
+            <div className="w-1/2 flex justify-center items-center dark:bg-white bg-[#E3E3E4] rounded-lg py-8 ">
               <Image
                 alt="instagram"
                 src="/images/Vector.png"
@@ -73,7 +73,7 @@ const ContactUsPage = () => {
                 height={80}
               />
             </div>
-            <div className="w-1/2 flex justify-center items-center bg-white rounded-lg py-8 ">
+            <div className="w-1/2 flex justify-center items-center dark:bg-white bg-[#E3E3E4] rounded-lg py-8 ">
               <Image
                 alt="instagram"
                 src="/images/skill-icons_instagram.png"
@@ -84,7 +84,7 @@ const ContactUsPage = () => {
           </div>
           <div className="flex gap-3">
             {" "}
-            <div className="w-1/2 flex justify-center items-center bg-white rounded-lg py-8 ">
+            <div className="w-1/2 flex justify-center items-center dark:bg-white bg-[#E3E3E4] rounded-lg py-8 ">
               <Image
                 alt="instagram"
                 src="/images/skill-icons_gmail-light.png"
@@ -92,7 +92,7 @@ const ContactUsPage = () => {
                 height={80}
               />
             </div>
-            <div className="w-1/2 flex justify-center items-center bg-white rounded-lg py-8 ">
+            <div className="w-1/2 flex justify-center items-center dark:bg-white bg-[#E3E3E4] rounded-lg py-8 ">
               <Image
                 alt="instagram"
                 src="/images/skill-icons_instagram.png"
@@ -102,7 +102,7 @@ const ContactUsPage = () => {
             </div>
           </div>
         </div>
-        <div className="md:w-1/2 lg:w-3/5 flex flex-col gap-5 justify-center text-lg text-white  p-10 sm:p-20 text-justify">
+        <div className="md:w-1/2 lg:w-3/5 flex flex-col gap-5 justify-center text-lg text-[#232323] dark:text-white p-10 sm:p-20 text-justify">
           <p>
             علاوه بر ارسال نظرات شما میتوانید ما را در شبکه های اجتماعی دنبال
             کنید و با ما در ارتباط باشید.
