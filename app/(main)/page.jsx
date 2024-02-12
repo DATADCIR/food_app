8941c81ef21a3e256969d053f1d151558cdbf67c
Amin <129054689+za-amin-rei@users.noreply.github.com>
2024-02-12 09:32:57 +0330

    light mod store

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
         <div className="header flex flex-col md:flex-row items-center relative gap-y-20">
           <div className="flex flex-col gap-6 sm:gap-3 w-full md:w-1/3">
             <h1 className="text-yellow-light text-3xl">کافه رستوران ساندی</h1>
-            <p className="text-white dark:text-yellow-400 text-lg">
+            <p className="text-black dark:text-yellow-400 text-lg">
               تجربه بهترین لحظات در کنار دوستان و خانواده در کنار ما ، امیدواریم
               لذت ببرید!؟
             </p>
@@ -51,7 +51,7 @@ export default function Home() {
           {[...Array(6)].map((index) => (
             <div
               key={index}
-              className="relative flex flex-col justify-center items-center w-88 h-44 rounded-se-3xl rounded-es-3xl bg-gray-light"
+              className="relative flex flex-col justify-center items-center w-88 h-44 rounded-se-3xl rounded-es-3xl dark:bg-gray-light bg-[#E3E3E4]"
             >
               <Image
                 src="/images/restaurant-food_2.png"
